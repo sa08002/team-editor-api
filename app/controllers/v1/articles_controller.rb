f81bd753ccbd20634f64a96bcb53715aa14be6bf
大西幹則 <1078sa08002@gmail.com>
2020-07-31 09:39:23 +0900

    articles_controller 記事投稿機能の実装

--- app/controllers/v1/articles_controller.rb
+++ app/controllers/v1/articles_controller.rb
@@ -6,8 +6,20 @@ class V1::ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
-    render json: @article
+    article = Article.find(params[:id])
+    render json: article
   end
+  
+  def create
+    article = current_v1_user.articles.create!(article_params)
+    render json: article
+  end
+
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
 
 end
