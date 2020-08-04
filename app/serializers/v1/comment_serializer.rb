3ba1cb3e02520f3ecfb681fb4c1a7d196fcf9304
koniman210 <koniman1123@gmail.com>
2020-08-04 15:57:17 +0900

    カラムの修正

--- app/serializers/v1/comment_serializer.rb
+++ app/serializers/v1/comment_serializer.rb
@@ -1,5 +1,5 @@
 class V1::CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content, :user, :article, :created_at, :updated_at
+  attributes :id, :content, :created_at, :updated_at
   belongs_to :user
   belongs_to :article
 end
