28803dbb3aab001a4dcdc61d07a0a94a2dbab1b6
koniman210 <koniman1123@gmail.com>
2020-08-12 08:07:39 +0900

    article_likesコントローラーを作成しcreate,destroyメソッドを追加

---
+++ app/controllers/v1/articles/article_likes_controller.rb
@@ -0,0 +1,15 @@
+class V1::Articles::ArticleLikesController < V1::BaseApiController
+
+  def create
+    article = Article.find(params[:article_id])
+    article_like = current_user.article_likes.new
+    article_like.article= article
+    article_like.save!
+  end
+  
+  def destroy
+    article = Article.find(params[:article_id])
+    article_like = current_user.article_likes.find_by(article_id: article.id)
+    article_like.destroy!
+  end
+end
