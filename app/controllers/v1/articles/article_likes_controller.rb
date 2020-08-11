000df5478a36568ff3e5e62dbcd7b25e23277c69
koniman210 <koniman1123@gmail.com>
2020-08-12 08:52:37 +0900

    rubocop修正

--- app/controllers/v1/articles/article_likes_controller.rb
+++ app/controllers/v1/articles/article_likes_controller.rb
@@ -1,12 +1,11 @@
 class V1::Articles::ArticleLikesController < V1::BaseApiController
-
   def create
     article = Article.find(params[:article_id])
     article_like = current_user.article_likes.new
-    article_like.article= article
+    article_like.article = article
     article_like.save!
   end
-  
+
   def destroy
     article = Article.find(params[:article_id])
     article_like = current_user.article_likes.find_by(article_id: article.id)
