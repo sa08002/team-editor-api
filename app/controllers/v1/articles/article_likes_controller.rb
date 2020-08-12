02b9f4e16ede1c94f628204bb45c730d0c52a557
koniman210 <koniman1123@gmail.com>
2020-08-12 10:48:12 +0900

    jsonを返すように修正

--- app/controllers/v1/articles/article_likes_controller.rb
+++ app/controllers/v1/articles/article_likes_controller.rb
@@ -4,11 +4,13 @@ class V1::Articles::ArticleLikesController < V1::BaseApiController
     article_like = current_user.article_likes.new
     article_like.article = article
     article_like.save!
+    render json: article_like
   end
 
   def destroy
     article = Article.find(params[:article_id])
     article_like = current_user.article_likes.find_by(article_id: article.id)
     article_like.destroy!
+    render json: article_like
   end
 end
