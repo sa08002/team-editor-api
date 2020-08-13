311ef076bd2edc908c3245be63a5dc61079ec3be
koniman210 <koniman1123@gmail.com>
2020-08-13 19:24:18 +0900

    comment_likeモデルのテストを作成

--- spec/models/comment_like_spec.rb
+++ spec/models/comment_like_spec.rb
@@ -21,5 +21,36 @@
 require "rails_helper"
 
 RSpec.describe CommentLike, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "必要な値がある場合" do
+    let(:comment_like) { build(:comment_like) }
+
+    it "コメントにいいねができる" do
+      expect(comment_like).to be_valid
+    end
+  end
+
+  context "comment が空の場合" do
+    let(:comment_like) { build(:comment_like, comment: nil) }
+    it "いいねできない" do
+      comment_like.valid?
+      expect(comment_like.errors.messages[:comment]).to include "must exist"
+    end
+  end
+
+  context "user が空の場合" do
+    let(:comment_like) { build(:comment_like, user: nil) }
+    it "いいねできない" do
+      comment_like.valid?
+      expect(comment_like.errors.messages[:user]).to include "must exist"
+    end
+  end
+
+  context "すでにいいねしている場合" do
+    let!(:comment_liked) { create(:comment_like) }
+    let(:comment_like) { build(:comment_like, user: comment_liked.user, comment: comment_liked.comment) }
+    it "いいねができない" do
+      comment_like.valid?
+      expect(comment_like.errors.messages[:comment_id]).to include "has already been taken"
+    end
+  end
 end
