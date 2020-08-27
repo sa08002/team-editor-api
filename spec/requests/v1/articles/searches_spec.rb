4ceacbc5e0fcd7154c6775191580784525e89509
大西幹則 <1078sa08002@gmail.com>
2020-08-27 15:40:06 +0900

    テストコード修正

--- spec/requests/v1/articles/searches_spec.rb
+++ spec/requests/v1/articles/searches_spec.rb
@@ -1,14 +1,25 @@
 require "rails_helper"
 
 RSpec.describe "V1::Articles", type: :request do
-  describe "GET /v1/articles/searches=Java" do
-    subject { get("/v1/articles/searches?search=Java") }
+  before do
+    create(:article, title: "楽しいJava")
+    create(:article, title: "楽しいRuby")
+    create(:article, title: "楽しいJavascript")
+    create(:article, title: "楽しいHTML")
+    create(:article, title: "楽しいCSS")
+    create(:article, title: "楽しいC#")
+    create(:article, title: "楽しいC++")
+    create(:article, title: "楽しいPython")
+    create(:article, title: "楽しいCOBOL")
+    create(:article, title: "楽しいSwift")
+  end
 
-    context "検索結果でデータを取得できた時" do
-      let!(:article1) { create(:article, title: "楽しいJava") }
-      let!(:article2) { create(:article, title: "楽しいRuby") }
-      let!(:article3) { create(:article, title: "楽しいJavascript") }
-      it "取得したデータ一覧が表示される" do
+  describe "GET /v1/articles/searches?q=Java" do
+    subject { get("#{v1_articles_searches_path}?q=#{query}") }
+
+    context "データが存在する状態で Java で検索したとき" do
+      let(:query) { "Java" }
+      it "Java を含んだデータが表示される" do
         subject
         res = JSON.parse(response.body)
         expect(res.length).to eq 2
@@ -17,29 +28,25 @@ RSpec.describe "V1::Articles", type: :request do
     end
   end
 
-  describe "GET /v1/articles/searches?search=" do
-    subject { get("/v1/articles/searches?search=") }
+  describe "GET /v1/articles/searches?q=" do
+    subject { get("#{v1_articles_searches_path}?q=#{query}") }
 
     context "検索項目が空の場合" do
-      let!(:article1) { create(:article, title: "楽しいJava") }
-      let!(:article2) { create(:article, title: "楽しいRuby") }
-      let!(:article3) { create(:article, title: "楽しいJavascript") }
+      let(:query) { "" }
       it "全データが表示される" do
         subject
         res = JSON.parse(response.body)
-        expect(res.length).to eq 3
+        expect(res.length).to eq 10
         expect(response).to have_http_status(:ok)
       end
     end
   end
 
-  describe "GET /v1/articles/searches?search=PHP" do
-    subject { get("/v1/articles/searches?search=PHP") }
+  describe "GET /v1/articles/searches?q=PHP" do
+    subject { get("#{v1_articles_searches_path}?q=#{query}") }
 
-    context "検索項目と一致するデータがない場合" do
-      let!(:article1) { create(:article, title: "楽しいJava") }
-      let!(:article2) { create(:article, title: "楽しいRuby") }
-      let!(:article3) { create(:article, title: "楽しいJavascript") }
+    context "一致するデータがない PHP で検索したとき" do
+      let(:query) { "PHP" }
       it "データが表示されない" do
         subject
         res = JSON.parse(response.body)
